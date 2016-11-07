1a32de70f0044fdf072e030c37575c22ffdfd6df
天雨流芳 <ning.chen@zeropartner.com>
2016-11-07 19:02:42 +0800

    webpack + vue + babel

---
+++ code/client/blog-admin-self/app/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>我从崖边跌落</li>
+      <li>落入星空辽阔</li>
+      <li>银河不清不浊</li>
+      <li>不知何以摆脱</li>
+    </ul>
+  </div>
+</template>
+<style>
+  li {
+    list-style: none;
+  }
+</style>
