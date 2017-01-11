6bc4629fa10e37042ddfff5cc1f7abc1e3938434
天雨流芳 <ning.chen@zeropartner.com>
2017-01-11 18:44:45 +0800

    init parallelTest.cs

---
+++ code/server/JIFUniverse/JIF.Core.Test/ParallelStudyTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace JIF.Core.Test
+{
+    [TestClass]
+    public class ParallelStudyTest
+    {
+
+        [TestMethod]
+        public void Create_Thread()
+        {
+            Thread t = new Thread(WriteY);
+            t.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("x-" + i);
+            }
+        }
+
+        private void WriteY()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("y-" + i);
+            }
+        }
+    }
+}
